26258ccb973103284f32b5066a23637da4feac04
rajat-scienaptic <rajat.meena@scienaptic.com>
2020-10-27 20:14:25 +0530

    build directory added

---
+++ src/main/resources/build/precache-manifest.4b235ca2e207c91f91858ca193d2665c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30225d4292b0cd14b6f3e41553a1bce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68a4175b2cd4dbbc1936",
+    "url": "./static/css/2.df04db45.chunk.css"
+  },
+  {
+    "revision": "af07e50b15fb9f32812e",
+    "url": "./static/css/main.34365ba0.chunk.css"
+  },
+  {
+    "revision": "68a4175b2cd4dbbc1936",
+    "url": "./static/js/2.bab7cef9.chunk.js"
+  },
+  {
+    "revision": "af07e50b15fb9f32812e",
+    "url": "./static/js/main.65a42fc2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "aee7ce1a2bdc18526ed5f4ce29de2301",
+    "url": "./static/media/Hp.aee7ce1a.png"
+  },
+  {
+    "revision": "dbd791b7c7030a09a32b2db68fc4472a",
+    "url": "./static/media/hpsimplified_lt-webfont.dbd791b7.ttf"
+  }
+]);
